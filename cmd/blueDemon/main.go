f0a1a1abaa3c70e3e1baf7cd6b7b1bb39b131e62
x0y14 <ya01ma08@gmail.com>
2023-11-11 17:48:58 +0900

    ゲームのベースを追加

--- cmd/blueDemon/main.go
+++ cmd/blueDemon/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"blueDemon"
+	"github.com/hajimehoshi/ebiten/v2"
+)
 
+func main() {
+	ebiten.SetWindowSize(blueDemon.ScreenWidth, blueDemon.ScreenHeight)
+	ebiten.SetWindowTitle("BlueDemon")
+	if err := ebiten.RunGame(&blueDemon.Game{}); err != nil {
+		panic(err)
+	}
 }
--- game.go
+++ game.go
@@ -1,1 +1,22 @@
 package blueDemon
+
+import "github.com/hajimehoshi/ebiten/v2"
+
+const (
+	ScreenWidth  = 640
+	ScreenHeight = 640
+)
+
+type Game struct {
+}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return ScreenWidth, ScreenHeight
+}
